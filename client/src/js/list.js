3c30d452681545eaba3c81fd286e2980f57903ab
yaohao <caocaojianke@gmail.com>
2018-06-05 11:03:46 +0800

    第一修改

--- client/src/js/list.js
+++ client/src/js/list.js
@@ -3,6 +3,39 @@ require(["config"], function () {
 
         $(".header").load("navheader.html")
 
+        $(window).ready(function () {
+
+
+            console.log($(".nav_list"))
+
+            //li标签切换功能;
+            $(".nav_list").children("li").on("mouseover", function () {
+                $(".nav_list").children("li").children("div").css("display", "none")
+                $(this).children("div").css("display", "block");
+                $(".list").css("display", "block")
+
+            }).mouseout(function () {
+                $(".nav_list").children("li").children("div").css("display", "none")
+
+            })
+            $(".list").mouseover(function () {
+                $(".list").css("display", "block")
+
+            })
+            $(".nav").mouseout(function () {
+                $(".list").css("display", "none")
+            })
+            //分割区----------------------------------------------------------------------
+
+
+
+
+
+
+        })
+
+
+
     })
 
 })
